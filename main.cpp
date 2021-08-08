285dbf04632b1b24eea06934ff6cc69d310fe937
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2021-08-08 21:40:36 +1000

    First check-in

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "include/ise102.h"
+using namespace std;
+
+int getGuessFromUser()
+{
+  string error = "";
+  int guess = -1;
+  do
+  {
+    if (error != "") print(error);
+    print("Enter a number betwixt 10 and 15 > ");
+    cin >> guess;
+    
+    if(guess >=10 && guess <= 15)
+    {
+      error = "";
+    } else
+    {
+      error = "Welp, you had one job. Try again.\n";
+    }
+  }
+  while (error != "");
+  return guess;
+}
+
+int main() {
+  clearScreen();
+
+  print("Welcome to guessy world!\n\n");
+
+
+  int guess = getGuessFromUser();
+
+  auto title_style = makeStyle( color::dark_violet,
+                                color::powder_blue,
+                                emphasis::bold ); 
+  
+  std::string name = "Damo";
+  print(title_style, "So, {0}.. You're going with {1}, is that right?\n", name, guess);
+  delay(500);
+  print("Ok mate.\n\n");
+
+  // Using fmt's print. 
+  
+  // Foreground and background colours plus, if you're on linus, emphasise with bold/underline etc.
+/*  auto error_style = makeStyle( color::beige,
+                                color::crimson,
+                                emphasis::bold);
+  */
+}
